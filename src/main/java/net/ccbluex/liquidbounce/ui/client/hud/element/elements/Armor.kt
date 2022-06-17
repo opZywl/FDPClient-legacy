a438b149124e86d610956ee3f7d2dbf0f1eb9291
XinHai <102407179+afdera@users.noreply.github.com>
2022-06-17 21:45:06 +0800

    Update New UI Armor

--- src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Armor.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/Armor.kt
@@ -1,66 +1,114 @@
-/*
- * FDPClient Hacked Client
- * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
- * https://github.com/UnlegitMC/FDPClient/
- */
 package net.ccbluex.liquidbounce.ui.client.hud.element.elements
 
 import net.ccbluex.liquidbounce.ui.client.hud.element.Border
 import net.ccbluex.liquidbounce.ui.client.hud.element.Element
 import net.ccbluex.liquidbounce.ui.client.hud.element.ElementInfo
-import net.ccbluex.liquidbounce.ui.client.hud.element.Side
+import net.ccbluex.liquidbounce.ui.font.Fonts
+import net.ccbluex.liquidbounce.utils.render.RenderUtils
+import net.ccbluex.liquidbounce.value.FloatValue
+import net.ccbluex.liquidbounce.value.IntegerValue
 import net.ccbluex.liquidbounce.value.ListValue
+import net.minecraft.block.material.Material
+import net.minecraft.client.Minecraft
 import net.minecraft.client.renderer.GlStateManager
 import org.lwjgl.opengl.GL11
+import java.awt.Color
+import kotlin.jvm.internal.Intrinsics
 
-/**
- * CustomHUD Armor element
- *
- * Shows a horizontal display of current armor
- */
 @ElementInfo(name = "Armor")
-class Armor(
-    x: Double = -8.0,
-    y: Double = 57.0,
-    scale: Float = 1F,
-    side: Side = Side(Side.Horizontal.MIDDLE, Side.Vertical.DOWN)
-) : Element(x, y, scale, side) {
+class Armor : Element() {
+    var colorModeValue = ListValue("Text-Color", arrayOf("Custom", "Astolfo"), "Custom")
+    var brightnessValue = FloatValue("Brightness", 1.0f, 0.0f, 1.0f)
+    var redValue = IntegerValue("Text-R", 255, 0, 255)
+    var greenValue = IntegerValue("Text-G", 255, 0, 255)
+    var blueValue = IntegerValue("Text-B", 255, 0, 255)
+    var newRainbowIndex = IntegerValue("NewRainbowOffset", 1, 1, 50)
+    var saturationValue = FloatValue("Saturation", 0.9f, 0.0f, 1.0f)
+    var speed = IntegerValue("AllSpeed", 0, 0, 400)
 
-    private val modeValue = ListValue("Alignment", arrayOf("Horizontal", "Vertical"), "Horizontal")
-    
-    /**
-     * Draw element
-     */
-    override fun drawElement(partialTicks: Float): Border {
-        if (mc.playerController.isNotCreative) {
-            val renderItem = mc.renderItem
-
+    override fun drawElement(partialTicks: Float): Border? {
+        var x2 = 0
+        val playercontrollermp = `access$getMc$p$s1046033730`().playerController
+        Intrinsics.checkExpressionValueIsNotNull(playercontrollermp, "mc.playerController")
+        if (playercontrollermp.isNotCreative) {
+            GL11.glPushMatrix()
+            val minecraft = `access$getMc$p$s1046033730`()
+            Intrinsics.checkExpressionValueIsNotNull(minecraft, "mc")
+            val renderItem = minecraft.renderItem
+            val isInsideWater = `access$getMc$p$s1046033730`().thePlayer.isInsideOfMaterial(Material.water)
             var x = 1
-            var y = 0
-
-            val mode = modeValue.get()
-
-            for (index in 3 downTo 0) {
-                val stack = mc.thePlayer.inventory.armorInventory[index] ?: continue
-
-                renderItem.renderItemIntoGUI(stack, x, y)
-                renderItem.renderItemOverlays(mc.fontRendererObj, stack, x, y)
-                if (mode.equals("Horizontal", true)) {
-                    x += 18
-                } else if (mode.equals("Vertical", true)) {
-                    y += 18
+            var i = 0
+            val y = if (isInsideWater) -10 else 0
+            val colorMode = colorModeValue.get()
+            val color = Color(
+                (redValue.get() as Number).toInt(),
+                (greenValue.get() as Number).toInt(),
+                (blueValue.get() as Number).toInt()
+            ).rgb
+            val rainbow = colorMode.equals("Rainbow", true)
+            var index = 0
+            val b0: Byte = 3
+            while (index <= b0) {
+                if (`access$getMc$p$s1046033730`().thePlayer.inventory.armorInventory[index] != null) {
+                    x2 += 20
                 }
+                ++index
             }
-
+            RenderUtils.drawRect(-2.0f, -4.0f, 2.0f + x2.toFloat(), 29.0f, Color(50, 50, 50, 60))
+            index = 3
+            val flag = false
+            while (index >= 0) {
+                val colorall = if (rainbow) 0 else if (colorMode.equals(
+                        "Astolfo",
+                        true
+                    )
+                ) RenderUtils.Astolfo(
+                    index * (speed.get() as Number).toInt(),
+                    (saturationValue.get() as Number).toFloat(),
+                    (brightnessValue.get() as Number).toFloat()
+                ) else color
+                val itemstack = `access$getMc$p$s1046033730`().thePlayer.inventory.armorInventory[index]
+                if (itemstack != null) {
+                    RenderUtils.drawGradientSidewaysV(
+                        x.toDouble(),
+                        0.0,
+                        x.toDouble() + 18.0,
+                        17.0,
+                        colorall,
+                        Color(140, 140, 140, 40).rgb
+                    )
+                    Fonts.font24.drawStringWithShadow(
+                        (itemstack.maxDamage - itemstack.itemDamage).toString(),
+                        x.toFloat() + 4.0f,
+                        20.0f,
+                        colorall
+                    )
+                    RenderUtils.drawRect(x.toFloat(), +25.0f, x.toFloat() + 18.0f, 26.0f, Color(140, 140, 140, 220).rgb)
+                    RenderUtils.drawRect(
+                        x.toFloat(),
+                        +25.0f,
+                        x.toFloat() + 18.0f * (itemstack.maxDamage - itemstack.itemDamage).toFloat() / itemstack.maxDamage.toFloat(),
+                        26.0f,
+                        colorall
+                    )
+                    renderItem.renderItemIntoGUI(itemstack, x + 1, y)
+                    x += 20
+                    ++i
+                }
+                --index
+            }
             GlStateManager.enableAlpha()
             GlStateManager.disableBlend()
             GlStateManager.disableLighting()
+            GlStateManager.disableCull()
+            GL11.glPopMatrix()
         }
+        return Border(-2.0f, -4.0f, 82.0f, 29.0f)
+    }
 
-        return if (modeValue.equals("Horizontal")) {
-            Border(0F, 0F, 72F, 17F)
-        } else {
-            Border(0F, 0F, 18F, 72F)
+    companion object {
+        fun `access$getMc$p$s1046033730`(): Minecraft {
+            return mc
         }
     }
 }
