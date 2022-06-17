3d2c8ede1124df16ef20c59de6ff4cf255d5f397
XinHai <102407179+afdera@users.noreply.github.com>
2022-06-17 20:07:14 +0800

    Update TargetStrafe.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/TargetStrafe.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/TargetStrafe.kt
@@ -74,6 +74,8 @@ class TargetStrafe : Module() {
             isEnabled = true
         }else {
             isEnabled = false
+            if (!thirdPersonViewValue.get()) return
+            mc.gameSettings.thirdPersonView = 3
         }
         return true
     }
