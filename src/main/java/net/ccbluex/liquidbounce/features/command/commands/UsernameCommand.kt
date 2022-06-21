1e2cc69649e5732c4bc9a9644b792fa12e7b8e66
Suicidemouse33 <107930391+Suicidemouse33@users.noreply.github.com>
2022-06-21 16:53:55 +0300

    remove shit client

--- src/main/java/net/ccbluex/liquidbounce/features/command/commands/UsernameCommand.kt
+++
@@ -1,9 +0,0 @@
-package net.ccbluex.liquidbounce.features.command.commands
-
-import net.ccbluex.liquidbounce.features.command.Command
-
-class UsernameCommand : Command("username", arrayOf("name")) {
-    override fun execute(args: Array<String>) {
-        alert("Username: " + mc.thePlayer.name)
-    }
-}
--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/SilentDisconnect.kt
+++
@@ -1,8 +0,0 @@
-package net.ccbluex.liquidbounce.features.module.modules.misc
-
-import net.ccbluex.liquidbounce.features.module.Module
-import net.ccbluex.liquidbounce.features.module.ModuleCategory
-import net.ccbluex.liquidbounce.features.module.ModuleInfo
-
-@ModuleInfo(name = "SilentDisconnect", category = ModuleCategory.MISC)
-class SilentDisconnect : Module()
